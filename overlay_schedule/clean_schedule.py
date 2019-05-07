d931b21f5ba9dc6acfc890e555570e3f2d3bb4ba
intotecho <intotecho@gmail.com>
2019-05-07 18:48:10 +1000

    Intermediate results for processing the overlay schedule into a CSV file.

--- overlay_schedule/clean_schedule.py
+++ overlay_schedule/clean_schedule.py
@@ -13,8 +13,9 @@ https://github.com/tabulapdf/tabula
 However there are a number of issues process the file, leading to
 1. Records with multiline text being  split over several rows of the CSV.
    These need to be detected and merged.
+2. Some fields get shifted into the wrong column.
 
-The objective of this script is to correct as manu errors as possible
+The objective of this script is to correct as many errors as possible
 so that the output is ready for uploading to BigQuery or other databases.
 
 """
@@ -50,6 +51,10 @@ input_df.columns = [
     'ProhibitedUsesPermited',
     'AboriginalHeritagePlace']
 '''
+exported_columns = [
+    'Overlay',
+    'HeritagePlace',
+    'PaintControls','InternalControls','TreeControls','FenceControls','IncludedInVHR','Included', 'VHR', 'Prohibited','AboriginalHeritagePlace', 'Status', 'Expiry']
 
 import pandas as pd
 import logging
@@ -66,19 +71,11 @@ logger.addHandler(ch)
 
 # ------------------ CONFIGURATION -------------------------------
 
-# Set your input file here
-# e.g. "revew_of_heritage_overlay_areas_2007_rev_may_2017.csv"
+# Set your input file here - this is the output of tabula.
+# e.g. "tabula-43_01s_yarra_lattice.csv"
 input_filename = "tabula-43_01s_yarra_lattice.csv"
 # Set your output file name here.
-output_filename = "tabula-43_01s_yarra_cleaned.csv"
-
-# Specify the columns we wish to read from the apps file. We want them all.
-schedule_columns_in = [
-    'PS', 'Heritage Place', 'External',
-    'Internal', 'Tree', 'Outbuildings',
-    'Included on the', 'Prohibited', 'Aboriginal'
-]
-
+output_filename = "heritage_schedule_cleaned.csv"
 
 # ------------------ DATA LOADING --------------------------------
 
@@ -111,36 +108,85 @@ input_df.rename(columns={
     'Prohibited': 'Prohibited',
     'Aboriginal': 'AboriginalHeritagePlace',
 }, inplace=True)
-input_df = input_df.iloc[:, : 9]
+input_df = input_df.iloc[:, : 13] # will need to drop 4 more after processing
+input_df['Included'] = '' # Fill with Yes or No from IncludedInVHR
+input_df['VHR'] = '' # Fill with the H reference from IncludedInVHR. 
+# WARNING. Does not support multiple H references numbers for HO350.
 
 input_count = len(input_df['Overlay'])
 
-# There are some rows which are completely empty so drop them first.
-#input_df.dropna(how='all', inplace=True)
+print ('=== SHIFT BLANK CELLS ===')
+'''
+Tabula has inserted some rows with nan. So the content is shifted to the right.
+This function will remocve the nan and shift the rest of the row to the left.
+All the real content has type string, not float.
+'''
+def shiftLeftWhenColumnEmpty(s):
+    firstColumn = 2
+    #count = 0
+    for c in range(firstColumn, len(s)-1):
+        if type(s[c]) == float: #np.nan:
+            s[c:] = s[c:].shift(-1)
+            #count = count + 1
+    #if count > 0:
+    #    print('\nShifted: ', count)
+    return s
 
+#input_df['keyrow'] = (input_df['Overlay'].str.match(r'.*HO\d*.*') != 0)
+#input_df = input_df[input_df.keyrow == True].apply(shiftLeftWhenColumnEmpty, axis=1)
+input_df = input_df.apply(shiftLeftWhenColumnEmpty, axis=1)
 
-''
-#Convert control  symbols  if needed.
+print ('=== REMOVE TITLE ROWS ===')
+'''
+The following indicate title rows from the PDF that can be deleted from the data
+
+PS,Heritage Place,External,Internal,Tree,Outbuildings,Included on the,Prohibited,Aboriginal,,,,
+map,,Paint,Alteration,Controls,or fences,Victorian Heritage,uses may be,heritage,,,,
+ref,,Controls,Controls,Apply?,which are not,Register under the,permitted?,place?,,,,
+,,Apply?,Apply?,exempt under,Heritage Act 2017 ?,,,,,,,
+,,,Clause 43.01-4,,,,,,,,,
+'''
+input_df['isTitleRow'] = \
+                            (input_df['Overlay'] == 'PS')   |  \
+                            (input_df['Overlay'] == 'map')  |  \
+                            (input_df['Overlay'] == 'ref')  |  \
+                            (input_df['PaintControls'] == 'Apply?') | \
+                            (input_df['InternalControls'] == 'Clause 43.01-4')
+                            
+input_df = input_df.drop(input_df[input_df.isTitleRow == True].index)
+input_df.reset_index(inplace=True, drop=True)
+
+# There are some rows which are completely empty so drop them.
+# input_df.dropna(how='all', inplace=True)
+
 input_df['Overlay'] = input_df['Overlay'].str.replace(r'\r', " ")
 input_df['HeritagePlace'] = input_df['HeritagePlace'].str.replace(r'\r', " ")
 input_df['PaintControls'] = input_df['PaintControls'].str.replace(r'\r', " ")
 input_df['InternalControls'] = input_df['InternalControls'].str.replace(r'\r', " ")
 input_df['TreeControls'] = input_df['TreeControls'].str.replace(r'\r', " ")
 input_df['IncludedInVHR'] = input_df['IncludedInVHR'].str.replace(r'\r', " ")
-
 input_df.fillna(" ", inplace=True)
 input_df['keyrow'] = (input_df['Overlay'].str.match(r'.*HO\d*.*') != 0)
-
 #input_df['FenceControls'] = input_df['FenceControls']
+regex = re.compile(
+    r'.*(HO\d*).*(Interim control).*(Expiry date):.*(\d\d/\d\d/\d\d\d\d)'
+)
 
-# === REMOVE HEADINGS === 
-input_df = input_df[input_df['Overlay'].str.match(r'PS|map|ref') == 0]
-input_df = input_df[input_df['Overlay'].str.match(r'PS map ref') == 0]
-input_df = input_df[input_df['PaintControls'].str.match(r'Controls|Apply') == 0]
-input_df = input_df[input_df['FenceControls'].str.match(r'Clause 43\.01\-4') == 0]
-input_df = input_df[input_df['FenceControls'].str.match(r'Controls|Apply') == 0]
-input_df.reset_index(inplace=True, drop=True)
-#print(input_df['FenceControls'].tolist())
+input_df['Expiry'] = input_df['Overlay'].str.replace(
+    regex,
+    "\\4",
+    regex=True)
+
+input_df['Status'] = input_df['Overlay'].str.replace(
+    regex,
+    "\\2",
+    regex=True)
+
+input_df['Overlay'] = input_df['Overlay'].str.replace(
+    regex,
+    "\\1",
+    regex=True)
+
 print ('=== HEAD ===')
 print (input_df.head(20))
 #print (input_df.shape)
@@ -149,7 +195,6 @@ print (input_df.head(20))
 #print (input_df['FenceControls'].tolist())
 
 print ('=== INTERMEDIATE ===')
-
 input_df.to_csv("{}".format(
     "intermediate.csv"),
     mode='w',
@@ -157,18 +202,23 @@ input_df.to_csv("{}".format(
     index=False,
     encoding='utf8')
 
+
 print ('=== LOOP ===')
+'''
+Slow loop...
+For each keyrow, merge content from subsequent rows
+'''
 length = len(input_df)
-# pattern = re.compile(r'H\d*')
 
 for i in range(0, length-1):
     #if i >= length:
     #    print ('Exiting at row {} {}'.format(i, input_df.loc[i]))
     #    break
-    heritagePlace = input_df.loc[i, 'HeritagePlace'].encode('utf-8')
-    includedInVHR = input_df.loc[i, 'IncludedInVHR'].encode('utf-8')
-    paint = input_df.loc[i, 'PaintControls'].encode('utf-8')
-    internal = input_df.loc[i, 'InternalControls'].encode('utf-8')
+    print(i)
+    heritagePlace = input_df.loc[i, 'HeritagePlace']  #  .encode('utf-8')
+    includedInVHR = input_df.loc[i, 'IncludedInVHR']  #  .encode('utf-8')
+    paint = input_df.loc[i, 'PaintControls']  # .encode('utf-8')
+    internal = input_df.loc[i, 'InternalControls']  # .encode('utf-8')
     # print(includedInVHR)
     isKeyRow = input_df.loc[i, 'keyrow']
     if isKeyRow == True:
@@ -176,12 +226,12 @@ for i in range(0, length-1):
             isfollowingRowKey = input_df.loc[d, 'keyrow']
             if isfollowingRowKey == False:
                 try:
-                    nextheritagePlace = input_df.loc[d, 'HeritagePlace'].encode('utf-8')
-                    nextVHR = input_df.loc[d, 'IncludedInVHR'].encode('utf-8')
-                    nextPaint = input_df.loc[d, 'PaintControls'].encode('utf-8')
-                    nextInternal = input_df.loc[d, 'InternalControls'].encode('utf-8')
-                    heritagePlace = heritagePlace  + nextheritagePlace #.encode('utf-8')
-                    includedInVHR = includedInVHR  + nextVHR #.encode('utf-8').replace(u"\u2019", "'")
+                    nextheritagePlace = input_df.loc[d, 'HeritagePlace']  #.encode('utf-8')
+                    nextVHR = input_df.loc[d, 'TreeControls'] + input_df.loc[d, 'IncludedInVHR'] # Tablula puts the H number into the wrong column E.g. HO67, HO292 
+                    nextPaint = input_df.loc[d, 'PaintControls'] #.encode('utf-8')
+                    nextInternal = input_df.loc[d, 'InternalControls'] #.encode('utf-8')
+                    heritagePlace = heritagePlace  + ' ' + nextheritagePlace #.encode('utf-8')
+                    includedInVHR = includedInVHR  + ' ' + nextVHR #.encode('utf-8').replace(u"\u2019", "'")
                     paint = paint  + nextPaint #.encode('utf-8').replace(u"\u2019", "'")
                     internal = internal  + nextInternal #.encode('utf-8').replace(u"\u2019", "'")
                     # print('{} {}'.format(i, includedInVHR))
@@ -195,49 +245,30 @@ for i in range(0, length-1):
         input_df.loc[i, 'HeritagePlace'] = heritagePlace.strip()
         input_df.loc[i, 'PaintControls'] = paint.strip()
         input_df.loc[i, 'InternalControls'] = internal.strip()
+        input_df.loc[i, 'IncludedInVHR'] = includedInVHR.strip()
 
-        includedInVHR = includedInVHR.strip()
-        
-        href =  re.search(r'(H\d*)', includedInVHR.decode("utf-8"))
-        if href: 
-            input_df.loc[i, 'VHR'] = href.group(1)
-        else:
-            input_df.loc[i, 'VHR'] = u''
-
-        included =  re.search(r'(\w*)', includedInVHR.decode("utf-8"))
-        if included: 
-            input_df.loc[i, 'IncludedInVHR'] = included.group(0)
-        else:
-            input_df.loc[i, 'IncludedInVHR'] = u' '
-
-        print(i, ', ', input_df.loc[i, 'Overlay'], ' ', input_df.loc[i, 'IncludedInVHR'], '+', input_df.loc[i, 'VHR'])
     else:
         pass
+
 # === DROP Non-Key  ROWS ====
 # input_df[input_df['keyrow'] ==True]
-input_df = input_df.drop(input_df[input_df.keyrow ==False].index)
+input_df = input_df.drop(input_df[input_df.keyrow == False].index)
 
-#input_df['IncludedInVHR'] = input_df['IncludedInVHR'].str.replace(r'\r', " ")
-#input_df['IncludedInVHR'] = input_df['IncludedInVHR'].str.replace(r'\n', " ")
-#input_df['VHR'] = input_df['IncludedInVHR']
-#input_df['VHR'] = input_df['IncludedInVHR'].str.decode('utf-8').replace(
-#    "(H\d*)",   "\1",  regex=True)
-#input_df['VHR'] = input_df['IncludedInVHR'].str.replace(
-##    r"(H\d*)",
-#    r"\\2")
-#
-#input_df['IncludedIn'] = ''
-#input_df['IncludedIn'] = input_df['IncludedInVHR'].str.match(r"No|\-|^$|",
-#    "\\1")
+#href =  re.search(r'(\w*) Ref No (H\d*)', includedInVHR )  #.decode("utf-8")
+
+input_df['Included'] = input_df['IncludedInVHR'].str.extract(r'(\w*).*')
+input_df['VHR'] = input_df['IncludedInVHR'].str.extract(r'\w* Ref No *(H\d*)')
 
 input_df.drop('keyrow', 1, inplace=True)
-# ---- save the results. ---- #
+
+# === save the results. === #
 output_df = input_df
 output_df.to_csv("{}".format(
     output_filename),
     mode='w',
     header=True,
     index=False,
+    columns=exported_columns,
     encoding='utf8')
 output_count = len(output_df['Overlay'])
 print('Cleaned {} lines to {} lines'.format(input_count, output_count))
