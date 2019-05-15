aa6331c38a7c87ef4713213c1e223c83bf8219a4
Chris Goodman <intotecho@netspace.net.au>
2019-05-15 15:18:17 +1000

    Scrape VHD index, place details and hashed files for statements of significance is working. First scraped version.

--- scrape_vhd/scrape_vhd/pipelines.py
+++ scrape_vhd/scrape_vhd/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os.path
 
 # Define your item pipelines here
 #
@@ -7,6 +8,30 @@
 
 
 class ScrapeVhdPipeline(object):
+
     def process_item(self, item, spider):
-        print(item)
+        try:
+            id = item['SoSHash']
+            filename = './/sos//{}.html'.format(id)
+            sospath = os.path.abspath(filename)
+            if not os.path.isfile(sospath) or os.path.getsize(sospath) == 0L:
+                print('Saving new SOS: {}'.format(id))
+                self.file = open(sospath, 'w')
+                self.file.write(item['StatementContent'])
+                self.file.close()
+            else:
+                print('Skipping duplicate SOS: {}'.format(id))
+            try:
+                del item['Summary']
+                del item['StatementContent']
+                del item['download_timeout']
+                del item['depth']
+                del item['download_slot']
+                del item['download_latency']
+            except KeyError, k:
+                print('EXCEPTION Deleting Key {}'.format(str(k)))
+                pass
+        except Exception, e:
+            print('EXCEPTION Saving new SOS: {} hash {} {}'.format(item['vhdplaceid'], id, str(e)))
+            exit()
         return item
--- scrape_vhd/scrape_vhd/settings.py
+++ scrape_vhd/scrape_vhd/settings.py
@@ -63,20 +63,21 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrape_vhd.pipelines.ScrapeVhdPipeline': 300,
-#}
+
+ITEM_PIPELINES = {
+    'scrape_vhd.pipelines.ScrapeVhdPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 1
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
